e38688c3c4ee753dbe598f6128a5aac0275386be
praveenmarish <praveenmarish0059@gmail.com>
2021-03-11 22:03:16 +0530

    initial commit

---
+++ controler/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from controler import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('video_feed', views.video_feed, name='video_feed'),
+    path('movement', views.movement, name='movement')
+    ]
---
+++ controler/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http.response import StreamingHttpResponse
+from controler.camera import VideoCamera, Movement
+# Create your views here.
+
+def index(request):
+	return render(request, 'home.html')
+
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def video_feed(request):
+	return StreamingHttpResponse(gen(VideoCamera()),
+					content_type='multipart/x-mixed-replace; boundary=frame')
+
+def movement(request):
+	return StreamingHttpResponse(gen(Movement()),
+					content_type='multipart/x-mixed-replace; boundary=frame')
